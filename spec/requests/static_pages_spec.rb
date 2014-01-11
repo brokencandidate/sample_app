34a4dfc9fdb60c2dd83c16802261e24016cf62d9
brokencandidate <brokencadidate@dev.null>
2014-01-11 17:21:41 +0000

    Add contact page, switch to postgres.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:page_title) {"Ruby on Rails Tutorial Sample App | "}
+
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -12,7 +15,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title(page_title + "Home")
     end
   end
 
@@ -25,7 +28,7 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title(page_title + "Help")
     end
   end
 
@@ -38,7 +41,21 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title(page_title + "About Us")
     end
   end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title(page_title + "Contact Us")
+    end
+  end
+
 end
